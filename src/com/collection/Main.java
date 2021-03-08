68aedb91fc331e739adc8a39bc9ab182bb36beec
moneer2020 <72842828+moneer2020@users.noreply.github.com>
2021-03-08 05:42:49 -0800

    My 4rd commit

--- src/com/collection/Main.java
+++ src/com/collection/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) throws InterruptedException {
 
-        for(int i = 0; i <6; i++) {
+        for(int i = 0; i <7; i++) {
             System.out.println("Hello Batch 8");
             Thread.sleep(2000);
         }
